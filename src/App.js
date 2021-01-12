907b46b424a744f54e82e292179964e19fabe7ac
Cassio Cabral <cassioscabral@gmail.com>
2021-01-12 21:30:22 +0100

    Initial scarab setup with react-tables

--- src/App.js
+++ src/App.js
@@ -1,42 +1,112 @@
 import './App.css';
 import poeNinjaApi from './api/poeninja'
-import { itemTypes, poeNinjaURLBuilder } from './utils/poeninja'
+import { itemsMeta, poeNinjaURLBuilder } from './utils/poeninja'
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useTable } from 'react-table'
+console.log('itemsMeta', itemsMeta);
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  // debugger
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
 
 function App() {
-  console.log('itemTypes', itemTypes);
   const [items, setItems] = useState({})
 
+  // "mounted"
   useEffect(() => {
     poeNinjaApi({
       method: 'GET',
-      url: poeNinjaURLBuilder('Oil')
+      url: poeNinjaURLBuilder('Scarab')
     }).then(res => {
       console.log('res', res);
       setItems({
-        oil: {
-          lines: res.data?.lines,
+        scarabs: {
+          items: res.data?.lines,
         }
       })
       return res
     })
   }, [])
+  const scarabColumns = React.useMemo(itemsMeta.scarabs.columns)
   return (
-    <div className="App">
+    <div className="App container mx-auto">
       <h1>POE Flip</h1>
       <hr />
-      <input type="number" name="oil.min" id="oil-min"/>
-      <hr />
-      <div className="items">
-        {items.oil?.lines.map(i => {
-          return (
-            <div className="oil-item" style={{display: 'flex'}}>
-              <div className="name" style={{marginRight: '10px'}}>{i.name}</div>
-              <div className="variation">{i.sparkline.totalChange}</div>
-            </div>
-          )
-        })}
-      </div>
+      { items?.scarabs?.items.length > 0 ?
+        <div className="scarabs-table">
+          <Styles>
+            <Table columns={scarabColumns} data={items.scarabs.items} />
+          </Styles>
+        </div>
+        :
+        <span>Scarabs not loaded</span>
+      }
     </div>
   );
 }
--- src/utils/poeninja.js
+++ src/utils/poeninja.js
@@ -1,3 +1,4 @@
+import React from 'react'
 // Category	API
 // Currency	https://poe.ninja/api/data/currencyoverview?league=Blight&type=Currency
 // Fragment	https://poe.ninja/api/data/currencyoverview?league=Blight&type=Fragment
@@ -21,14 +22,102 @@
 // Unique Accessories	https://poe.ninja/api/data/itemoverview?league=Blight&type=UniqueAccessory
 // Beasts	https://poe.ninja/api/data/itemoverview?league=Blight&type=Beast
 
-import POE_NINJA_BASE_URL from '../api/poeninja'
 
-const itemTypes = {
+// const itemNormalizer = {
+//   currency: (item) => ({
+
+//   }),
+//   scarabs: (item) => ({
+
+//   })
+// }
+
+const defaultColumns = (otherColumns = [], { league = 'Standard'} = {}) => [
+  ...[
+  {
+    Header: 'Name',
+    // accessor: 'name',
+    Cell: row => {
+      const originalItem = row.row.original
+      return (
+        <div className="item-name flex items-center justify-left">
+          { originalItem?.icon.length > 0 ?
+            <div className="item-icon mr-2">
+              <img src={originalItem.icon} alt={originalItem.name} />
+            </div>
+            :
+            <span/>
+          }
+          <div className="item-name">
+            { originalItem.name }
+          </div>
+        </div>
+      )
+    }
+  },
+  {
+    Header: 'Last 7 days',
+    accessor: 'sparkline.totalChange',
+    Cell: row => {
+      const { totalChange } = row.row.original.sparkline
+      return (
+        <div className={'item-change' + (totalChange > 0 ? ' text-green-600' : ' text-red-600')}>
+          {totalChange}
+        </div>
+      )
+    }
+  },
+  {
+    Header: 'C. Value',
+    Cell: row => {
+      return (
+        <div className="item-value flex justify-end items-center">
+          {row.row.original.chaosValue > 100 ? <div className="exalted-value mr-2">{row.row.original.exaltedValue}ex</div> : <span />}
+          <div className="chaos-value flex-auto justify-end flex items-center">
+            {row.row.original.chaosValue}
+            <img src="https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyRerollRare.png?scale=1&w=1&h=1" alt="Chaos Orb"/>
+          </div>
+        </div>
+      )
+    }
+  }
+  ],
+  ...otherColumns,
+  {
+    Header: 'Actions',
+    Cell: row => {
+      return (
+        <div className="actions">
+{/* https://www.pathofexile.com/trade/search/Standard?q={%22query%22:{%22filters%22:{},%22type%22:%22Winged%20Bestiary%20Scarab%22}} */}
+          <button
+            className="border-aqua border-2 p-2 rounded"
+            onClick={() => window.open(`https://www.pathofexile.com/trade/search/${league}?q={"query":{"filters":{},"type":"${row.row.original.name}"}}`, '_blank')}>
+          Search
+        </button>
+      </div>
+      )
+    }
+  }
+]
+
+const itemsMeta = {
   currency: {
     id: 'Currency',
+    endpoint: 'currencyoverview'
+  },
+  fragment: {
+    id: 'Fragment',
+    endpoint: 'currencyoverview'
   },
   oil: {
     id: 'Oil',
+  },
+  incubators: {
+    id: 'Incubators'
+  },
+  scarabs: {
+    id: 'Scarabs',
+    columns: defaultColumns
   }
 }
 const poeNinjaURLBuilder = (objectType, endpoint = 'itemoverview', league = 'Standard' ) => {
@@ -37,5 +126,5 @@ const poeNinjaURLBuilder = (objectType, endpoint = 'itemoverview', league = 'Sta
 
 export {
   poeNinjaURLBuilder,
-  itemTypes
+  itemsMeta
 }
